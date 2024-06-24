fc17bbadb9a65069bb14a22cf234299d6a73052c
Jannis Lauer <jannislauer@icloud.com>
2024-06-24 17:50:59 +0200

    Create spam random.calculation.py

---
+++ spam random.calculation.py
@@ -0,0 +1,19 @@
+import random
+import pyautogui as pg
+import time 
+
+time.sleep(3)
+len = 3
+w = ("+","-","*",":","% von ")
+
+for j in range(1):
+    for i in range(len):
+        pg.write(str(random.randint(0,9)))
+    
+    pg.write(random.choice(w))
+
+    for i in range(len):
+        pg.write(str(random.randint(0,9)))
+
+time.sleep(0.1)
+pg.press("enter")
